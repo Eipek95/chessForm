390e1f04739012a47d47f730846562aafd34d5ff
Emre <Emre@DESKTOP-THOD234>
2021-05-22 15:00:16 +0300

    Add project files.

---
+++ chessForm/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace chessForm
+{
+    partial class formChess
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formChess));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtlocationX = new System.Windows.Forms.TextBox();
+            this.txtLocationY = new System.Windows.Forms.TextBox();
+            this.btnBaslat = new System.Windows.Forms.Button();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblAdim = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(26, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(327, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Lutfen  Atın x ve y konumlarini giriniz(1-8 arasında)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(79, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "X:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(79, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Y:";
+            // 
+            // txtlocationX
+            // 
+            this.txtlocationX.Location = new System.Drawing.Point(106, 96);
+            this.txtlocationX.Name = "txtlocationX";
+            this.txtlocationX.Size = new System.Drawing.Size(138, 20);
+            this.txtlocationX.TabIndex = 3;
+            this.txtlocationX.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtlocationX_KeyPress);
+            // 
+            // txtLocationY
+            // 
+            this.txtLocationY.Location = new System.Drawing.Point(106, 137);
+            this.txtLocationY.Name = "txtLocationY";
+            this.txtLocationY.Size = new System.Drawing.Size(138, 20);
+            this.txtLocationY.TabIndex = 4;
+            this.txtLocationY.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtLocationY_KeyPress);
+            // 
+            // btnBaslat
+            // 
+            this.btnBaslat.Location = new System.Drawing.Point(132, 178);
+            this.btnBaslat.Name = "btnBaslat";
+            this.btnBaslat.Size = new System.Drawing.Size(75, 23);
+            this.btnBaslat.TabIndex = 5;
+            this.btnBaslat.Text = "Başlat";
+            this.btnBaslat.UseVisualStyleBackColor = true;
+            this.btnBaslat.Click += new System.EventHandler(this.btnBaslat_Click);
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(359, 32);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(496, 461);
+            this.flowLayoutPanel1.TabIndex = 9;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(29, 207);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(270, 159);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.Text = resources.GetString("textBox1.Text");
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.lblAdim);
+            this.panel1.Location = new System.Drawing.Point(877, 32);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(312, 461);
+            this.panel1.TabIndex = 11;
+            // 
+            // lblAdim
+            // 
+            this.lblAdim.AutoSize = true;
+            this.lblAdim.Location = new System.Drawing.Point(136, 17);
+            this.lblAdim.Name = "lblAdim";
+            this.lblAdim.Size = new System.Drawing.Size(0, 13);
+            this.lblAdim.TabIndex = 14;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(571, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 17);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Şema:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(1013, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 17);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Adımlar:";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // formChess
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1201, 534);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.btnBaslat);
+            this.Controls.Add(this.txtLocationY);
+            this.Controls.Add(this.txtlocationX);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "formChess";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Chesse Horse Move";
+            this.Load += new System.EventHandler(this.formChess_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtlocationX;
+        private System.Windows.Forms.TextBox txtLocationY;
+        private System.Windows.Forms.Button btnBaslat;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblAdim;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ chessForm/Form1.cs
@@ -0,0 +1,318 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace chessForm
+{
+    public partial class formChess : Form
+    {
+        static int[,] tahta = new int[DefineConstants.kareSayisi, DefineConstants.kareSayisi]; //satranc tahtasi 8*8lik
+        static int[] alt_x = { 0, 0, 0, 0, 0, 0, 0, 0 }; //gidilebilecek alternatif karelerin X konumlarini tutuyor
+        static int[] alt_y = { 0, 0, 0, 0, 0, 0, 0, 0 }; //gidilebilecek alternatif karelerin Y konumlarini tutuyor
+        static int[] alt_cikis = { 0, 0, 0, 0, 0, 0, 0, 0 }; //alternatiflerden cikislarin sayilarini tutuyor
+        static int[] fark_x = { -2, -1, 1, 2, 2, 1, -1, -2 }; //saat yonunde olusan farklar
+        static int[] fark_y = { 1, 2, 2, 1, -1, -2, -2, -1 };
+        static Label[,] _arrLbl = new Label[8, 8];//satranc tahtasi icin kullanilan label dizisi 8*8lik
+        static int i, j, sayac = 1, g, t = 1, saniye = 3;
+        static int xyer, yyer, x, y, hareket, say = 0;
+
+
+        public formChess()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnBaslat_Click(object sender, EventArgs e)
+        {
+            if (say == 0)
+            {
+                if (txtlocationX.Text == string.Empty || txtLocationY.Text == string.Empty)
+
+                {
+                    MessageBox.Show("Konum Boş olamaz");
+                    say--;
+                }
+                else
+                {
+                    xyer = Convert.ToInt32(txtlocationX.Text);
+                    xyer = xyer - 1;
+                    yyer = Convert.ToInt32(txtLocationY.Text);
+                    yyer = yyer - 1;
+                    if (xyer < 0 || xyer > DefineConstants.kareSayisi - 1 || yyer < 0 || yyer > DefineConstants.kareSayisi - 1)
+                    {
+                        MessageBox.Show("Lütfen 1 ile 8 arasında tam sayı giriniz...");
+                        say--;
+                    }
+                    else
+                    {
+                        x = xyer;
+                        y = yyer;
+
+                        while ((tahta[xyer, yyer]) == 0) //dongu,gidilecek kare ziyaret edilmedigi surece devam eder
+                        {
+                            tahta[xyer, yyer] = sayac++;
+                            alternatif_bul(xyer, yyer);
+                            alt_say_hesapla();
+                            g = yk_bul(); //g degiskeni yk_bul fonksiyonundan donen indisi tutuyor
+                            if (g != -1)
+                            {
+                                xyer = alt_x[g];
+                                yyer = alt_y[g];
+                            }
+
+
+                            for (i = 0; i < 8; i++) //her adimi ekrana yazdir.
+                            {
+                                for (j = 0; j < 8; j++)
+                                {
+                                    _arrLbl[i, j].Text = tahta[i, j].ToString();
+                                    lblAdim.Text += tahta[i, j].ToString() + " ";
+
+                                }
+                                lblAdim.Text += "\n";
+                            }
+                            lblAdim.Text += "\n\n";
+
+                            alt_dizi_sifirla();
+
+                        }
+                        hareket = sayac - 1;
+                        for (i = 0; i < 8; i++) //eger g nin degeri -1 ise ve hala cikis var ise(at sondan bir onceki hamlede ise)
+                        {
+                            if (xyer + fark_x[i] > -1 && xyer + fark_x[i] < DefineConstants.kareSayisi && yyer + fark_y[i] > -1 && yyer + fark_y[i] < DefineConstants.kareSayisi && tahta[(xyer + fark_x[i]), (yyer + fark_y[i])] == 0)
+                            {
+                                tahta[(xyer + fark_x[i]), (yyer + fark_y[i])] = sayac; //sayacin son degerini son kareye ata
+                                hareket++;
+                            }
+                        }
+
+                        for (i = 0; i < 8; i++) //son durumu ekrana yaz.
+                        {
+                            for (j = 0; j < 8; j++)
+                            {
+                                _arrLbl[i, j].Text = tahta[i, j].ToString();
+                                lblAdim.Text += tahta[i, j].ToString() + " ";
+                            }
+                            lblAdim.Text += "\n";
+                        }
+                        lblAdim.Text += ("\n\n");
+
+                    }
+                }
+            }
+            else
+            {
+                Application.Restart();
+                 
+            }
+            say++;
+            
+        }
+
+
+
+
+
+
+        //algoritmalar
+        public static void alternatif_bul(int x, int y)
+        { //atin bulundugu kareden gidebilecegi konumlari bulur,X konumlarini alt_x dizisine,Y konumlarini alt_y dizisine atar.
+            int i;
+            for (i = 0; i < 8; i++)
+            {
+                alt_x[i] = x + fark_x[i];
+                alt_y[i] = y + fark_y[i];
+                
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Interval = 1000;
+            tahtaGez();
+        }
+
+        public static void alt_say_hesapla()
+        { //alt_say_hesapla fonksiyonu alternatif karelerden gidilebilecek karelerin sayilarini (kareler daha once gezilmemis ve matris icinde //ise)alt_cikis dizisine atar.
+            int tmp_x;
+            int tmp_y;
+            for (i = 0; i < 8; i++)
+            {
+                if (alt_x[i] > -1 && alt_y[i] > -1 && alt_x[i] < DefineConstants.kareSayisi && alt_y[i] < DefineConstants.kareSayisi && (tahta[alt_x[i], alt_y[i]]) == 0) //alternatif cikis gezilmemis ve
+                {
+                    for (j = 0; j < 8; j++) // tahta icinde mi?
+                    {
+                        tmp_x = alt_x[i] + fark_x[j];
+                        tmp_y = alt_y[i] + fark_y[j];
+                        if (tmp_x > -1 && tmp_y > -1 && tmp_x < DefineConstants.kareSayisi && tmp_y < DefineConstants.kareSayisi && (tahta[tmp_x, tmp_y]) == 0) // alternatif cikis
+                        {
+                            alt_cikis[i]++; //gezilmemis ve tahta icinde ise cikisa ait alternatif sayisini        1 arttir
+                        }
+                    }
+                }
+            }
+        }
+
+        public static int yk_bul()
+        { //alt_cikis dizisinin 0 haric en kucuk elemaninin indisini dondurur.
+            int sayi = 9;
+            int ind = -1;
+            for (i = 0; i < 8; i++)
+            {
+                if (alt_cikis[i] != 0) //cikis 0 ise donguden cik
+                {
+                    if (sayi > alt_cikis[i])
+                    { //cikis 9 dan kucuk ise
+                        sayi = alt_cikis[i]; //sayiyi cikis sayisina esitle
+                        ind = i; //ind degiskenine indisi ata
+                    }
+                }
+            }
+            return ind; //indisi dondur
+        }
+
+        public static void alt_dizi_sifirla()
+        {
+           
+            for (i = 0; i < 8; i++)
+            {
+                alt_cikis[i] = 0;
+            }
+        }
+        //kontroller
+        private void txtlocationX_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void txtLocationY_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        //form acilmasi
+        private void formChess_Load(object sender, EventArgs e)
+        {
+            tahtaOlustur();
+            timer1.Start();
+        }
+
+        //label kullanarak ile satranc tahtası
+        public void tahtaOlustur()
+        {
+            int  yer = 0;
+            for (i = 0; i < 8; i++)
+            {
+
+                for (j = 0; j < 8; j++)
+                {
+                    if (i % 2 == 0)
+                    {
+
+                        if (yer == 1)
+                        {
+                            _arrLbl[i, j] = new Label();
+                            _arrLbl[i, j].BackColor = Color.Black;
+                            _arrLbl[i, j].ForeColor = Color.Black;
+                            _arrLbl[i, j].Size = new Size(50, 50);
+                            _arrLbl[i, j].TextAlign = ContentAlignment.MiddleCenter;
+                            _arrLbl[i, j].Location = new Point(j * 50, i * 50);
+                            flowLayoutPanel1.Controls.Add(_arrLbl[i, j]);
+
+                            yer = 0;
+                        }
+
+                        else
+                        {
+                            yer++;
+                            _arrLbl[i, j] = new Label();
+                            _arrLbl[i, j].Size = new Size(50, 50);
+                            _arrLbl[i, j].BackColor = Color.White;
+                            _arrLbl[i, j].ForeColor = Color.White;
+                            _arrLbl[i, j].TextAlign = ContentAlignment.MiddleCenter;
+                            _arrLbl[i, j].Location = new Point(j * 50, i * 50);
+                            flowLayoutPanel1.Controls.Add(_arrLbl[i, j]);
+
+                        }
+
+
+                    }
+                    else
+
+                    {
+                        if (yer == 0)
+
+                        {
+
+                            _arrLbl[i, j] = new Label();
+                            _arrLbl[i, j].BackColor = Color.Black;
+                            _arrLbl[i, j].TextAlign = ContentAlignment.MiddleCenter;
+                            _arrLbl[i, j].ForeColor = Color.Black;
+                            _arrLbl[i, j].Size = new Size(50, 50);
+                            _arrLbl[i, j].Location = new Point(j * 50, i * 50);
+                            flowLayoutPanel1.Controls.Add(_arrLbl[i, j]);
+                            yer++;
+
+                        }
+                        else
+                        {
+                            yer = 0;
+                            _arrLbl[i, j] = new Label();
+                            _arrLbl[i, j].Size = new Size(50, 50);
+                            _arrLbl[i, j].BackColor = Color.White;
+                            _arrLbl[i, j].TextAlign = ContentAlignment.MiddleCenter;
+                            _arrLbl[i, j].ForeColor = Color.White;
+                            _arrLbl[i, j].Location = new Point(j * 50, i * 50);
+                            flowLayoutPanel1.Controls.Add(_arrLbl[i, j]);
+                        }
+
+                    }
+                }
+            }
+        }
+
+        //tahta üzerinde gezme kodu
+        public void tahtaGez()
+        {
+
+            for (i = 0; i < 8; i++)
+            {
+                for (j = 0; j < 8; j++)
+                {
+                    if (t >= 1 && t <= 64)
+                    {
+                        if (_arrLbl[i, j].Text == t.ToString())
+                        {
+
+                            _arrLbl[i, j].Image = Image.FromFile(Application.StartupPath + @"\horse.png");
+
+                            saniye--;
+                            if (saniye == 0)
+                            {
+
+                                _arrLbl[i, j].Image = null;
+                                saniye = 3;
+                                t++;
+                            }
+
+                        }
+                    }
+
+                }
+            }
+        }
+
+        static class DefineConstants
+        {
+            public const int kareSayisi = 8;
+        };
+
+    }
+}
